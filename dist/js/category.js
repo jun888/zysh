ba154e180b9e5fb312602efc7939315d4b6f3857
zj <zj@qq.com>
2016-09-06 16:16:38 +0800

    添加新页面

---
+++ dist/js/category.js
@@ -0,0 +1,39 @@
+webpackJsonp([0],{
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _reactDom = __webpack_require__(34);
+
+	var _CommonHead = __webpack_require__(172);
+
+	var _CommonFoot = __webpack_require__(173);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	__webpack_require__(174);
+	__webpack_require__(178);
+
+	(0, _reactDom.render)(_react2.default.createElement(
+		"div",
+		null,
+		_react2.default.createElement(_CommonHead.CommonHeader, null),
+		_react2.default.createElement(_CommonFoot.CommonFoot, null)
+	), document.getElementById("pageindex"));
+
+/***/ },
+
+/***/ 178:
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ }
+
+});
